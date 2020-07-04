1dcd9f90c31fc57ad2d2b98bb62e0af2eaed25f4
salma-275 <66432974+salma-275@users.noreply.github.com>
2020-07-04 17:27:08 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,33 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+    if (nime) {
+        console.log("hello " + name);   
+    }else 
+    console.log("hello");
+    
+}
+greet ( );
+greet = (" salma");
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+//     switch( %2 ==1) {
+//         case 1:
+//           console.log("true")
+//           break;
+//         default:
+//            console.log(" false ")
+//       }
+// }
+if (%2 == 1) {
+    return true;   
+}else 
+    return false;
+}
+isOdd ( ); 
